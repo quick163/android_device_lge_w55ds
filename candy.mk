fbc8082c773fd24f7f19c8b6635336035d2092a7
quick163 <freesens2@yandex.ru>
2016-08-13 21:37:25 +0400

    Candyfy

--- candy.mk
+++ candy.mk
@@ -1,5 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/w55ds/device_w55ds.mk)
 
@@ -9,7 +9,7 @@ TARGET_SCREEN_HEIGHT := 800
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L65
-PRODUCT_NAME := cm_w55ds
+PRODUCT_NAME := candy_w55ds
 PRODUCT_MODEL := LG-D285
 PRODUCT_DEVICE := w55ds
 PRODUCT_MANUFACTURER := LGE
